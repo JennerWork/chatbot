c11fa5f88791e9e867e56ad21fdae1d23e10822a
JennerWork <jenner.j.hou@outlook.com>
2025-03-25 17:36:21 +0800

    feat:database

--- pkg/db/postgres.go
+++ pkg/db/postgres.go
@@ -1,1 +1,26 @@
 package db
+
+import (
+	"github.com/JennerWork/chatbot/internal/config"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+// Init 初始化PostgreSQL数据库连接
+func Init(cfg *config.DatabaseConfig) error {
+	var err error
+	DB, err = gorm.Open(postgres.Open(cfg.DSN()), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// GetDB 获取数据库连接实例
+func GetDB() *gorm.DB {
+	return DB
+}
